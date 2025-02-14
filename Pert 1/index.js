8b54a44455a50810a2e45c3a75cfd2267ae61be4
piowarior <muhamadrohisuliman@gmail.com>
2025-02-14 19:52:27 +0700

    pert 1

---
+++ Pert 1/index.js
@@ -0,0 +1,6 @@
+document.getElementById("tombol").addEventListener("click", function() {
+    document.getElementById("judul").innerText = "Teks Berubah!";
+    alert("Kamu menekan tombol!");
+});
+
+document.getElementById("judul").innerText = "Halo, JavaScript!";
