8681634874fa1842bdf1e04cf8823bcec41483e7
piowarior <muhamadrohisuliman@gmail.com>
2025-02-24 19:18:42 +0700

    pert6 Belajar Java Script

---
+++ pert6/tes.js
@@ -0,0 +1,40 @@
+
+// let j = 5;
+// while (j <= 10) {
+//    console.log("ok");
+   
+//    j++;
+// }
+
+// let i = 1;
+
+// do {
+//     i++;
+//     console.log("helo");
+// } while (i <= 10);
+
+// let j = 1;
+
+// while (j < 10){
+//     j++;
+//     console.log("ik");
+// }
+
+
+let x1 = 1;
+
+while(x1 <= 20){
+    x1++;
+    if (x1 % 2 == 0) {
+        console.log(x1);
+    }
+}
+
+
+let f = 1;
+
+while(f <= 5){
+    f++;
+    console.log(f);
+}
+
---
+++ pert6/tipe_data.js
@@ -0,0 +1,16 @@
+let a = 123;
+
+console.log(a);
+
+let b = "hanjuk";
+
+console.log(b);
+
+
+let c = ["yuji","molean","nunu","muli"];
+
+for (let i = 0; i <= 3; i++) {
+    
+    console.log(c[i]);
+    
+}
