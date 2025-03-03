bb402af13f4b034cdeb3e20c2cd1230be879c618
piowarior <muhamadrohisuliman@gmail.com>
2025-03-03 13:38:22 +0700

    membuat tugas

---
+++ Pert1/tugas/tugas.js
@@ -0,0 +1,39 @@
+    let b = ""
+
+function ganti(){
+
+    let a = document.getElementById("kotak")
+ 
+    if(a.style.backgroundColor == "green" || b == "blue"){
+        a.style.backgroundColor = "red"
+        b = "red"
+    }else{
+            a.style.backgroundColor = "blue"
+         b = "blue"
+    }
+}
+
+// document.getElementById("kotak2")
+
+// document.getElementById("tombol2").addEventListener("click", document.getElementById("kotak2").style.backgroundColor = "red" )
+
+
+// document.getElementById("navbar").addEventListener("click", function(){
+//     console.log("masukk kedalam bubble");
+// })
+
+let index = document.getElementById("dropdown")
+
+index.style.display = "none"
+
+// document.getElementById("menuButton")
+
+function keluar(){
+
+    if(index.style.display == "none"){
+        index.style.display = "block"
+    }else{
+        index.style.display = "none"
+        cl
+    }
+}
