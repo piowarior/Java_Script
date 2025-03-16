b5262eb8a66e419667e116d799619c95726e7036
piowarior <muhamadrohisuliman@gmail.com>
2025-03-16 21:36:32 +0700

    belajar tugas

---
+++ coba/login.js
@@ -0,0 +1,21 @@
+let loginside = document.getElementsByClassName("login-side")[0]
+let loginutama = document.querySelector(".login-utama")
+let tombolregistrasi = document.getElementById("registrasi")
+let tombolregistrasi2 = document.getElementById("registrasi2")
+
+let ceklogin = 1
+function pindah() {
+    loginside.classList.toggle("pindah-login-side1")
+    loginutama.classList.toggle("pindah-login-utama1")
+    tombolregistrasi.classList.add("pindah-registrasi")
+    loginside.addEventListener("transitionend", function() {
+        loginside.classList.add("pindah-login-side2")
+        tombolregistrasi.classList.add("pindah-registrasi2")
+    })
+    tombolregistrasi.addEventListener("transitionend", function () {
+        tombolregistrasi2.classList.add("pindah2-registrasi")
+    })
+    
+}
+
+
