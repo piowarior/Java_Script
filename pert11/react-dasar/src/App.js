97ffb05c38479370399e558157809dc7b95dad76
piowarior <muhamadrohisuliman@gmail.com>
2025-09-22 14:58:18 +0700

    menambahkan pert11

---
+++ pert11/react-dasar/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="App">
+        <header className="App-header">
+          
+          <p>
+           test
+          </p>
+         
+        </header>
+      </div>
+        <h1>test1</h1>
+    </div>
+  );
+}
+
+export default App;
