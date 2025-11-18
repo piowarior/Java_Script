8ee43b260eecba078061edc8b5da19b75bdb63f1
piowarior <muhamadrohisuliman@gmail.com>
2025-11-18 21:25:26 +0700

    update new 17-11-2025

--- JS-Dasar/pert1/tugas/tugas.js
+++ JS-Dasar/pert1/tugas/tugas.js
@@ -1,35 +1,40 @@
-    let b = ""
+//     let b = ""
 
-function ganti(){
+// function ganti(){
 
-    let a = document.getElementById("kotak")
+//     let a = document.getElementById("kotak")
  
-    if(a.style.backgroundColor == "green" || b == "blue"){
-        a.style.backgroundColor = "red"
-        b = "red"
-    }else{
-            a.style.backgroundColor = "blue"
-         b = "blue"
-    }
+//     if(a.style.backgroundColor == "green" || b == "blue"){
+//         a.style.backgroundColor = "red"
+//         b = "red"
+//     }else{
+//             a.style.backgroundColor = "blue"
+//          b = "blue"
+//     }
+// }
+
+// // document.getElementById("kotak2")
+
+// // document.getElementById("tombol2").addEventListener("click", document.getElementById("kotak2").style.backgroundColor = "red" )
+
+
+// // document.getElementById("navbar").addEventListener("click", function(){
+// //     console.log("masukk kedalam bubble");
+// // })
+
+// let index = document.getElementById("dropdown")
+
+// // index.style.display = "none"
+
+// // document.getElementById("menuButton")
+
+// function keluar(){
+
+//     index.classList.toggle("show")
+// }
+
+let kotak = document.getElementById('kotak')
+
+function gantiwarna(){
+    kotak.style.backgroundColor = 'green'
 }
-
-// document.getElementById("kotak2")
-
-// document.getElementById("tombol2").addEventListener("click", document.getElementById("kotak2").style.backgroundColor = "red" )
-
-
-// document.getElementById("navbar").addEventListener("click", function(){
-//     console.log("masukk kedalam bubble");
-// })
-
-let index = document.getElementById("dropdown")
-
-// index.style.display = "none"
-
-// document.getElementById("menuButton")
-
-function keluar(){
-
-    index.classList.toggle("show")
-}
-
