6304188d4ce9bbe776c347761f3b72ac4aeb76ea
piowarior <muhamadrohisuliman@gmail.com>
2025-11-19 14:58:17 +0700

    update new 19-11-2025

--- JS-Dasar/pert2/tugas/index.js
+++ JS-Dasar/pert2/tugas/index.js
@@ -1,24 +1,26 @@
-let indexSekarang = 0;
-const gambar = document.getElementsByClassName("gambar");
-const totalGambar = gambar.length;
+// let indexSekarang = 0;
+// const gambar = document.getElementsByClassName("gambar");
+// const totalGambar = gambar.length;
 
-function updateSlider() {
-    const offset = -indexSekarang * 500; // 500 = lebar gambar
-    document.querySelector('.slider-track').style.transform = `translateX(${offset}px)`;
-}
+// function updateSlider() {
+//     const offset = -indexSekarang * 500; // 500 = lebar gambar
+//     document.querySelector('.slider-track').style.transform = `translateX(${offset}px)`;
+// }
 
-function nextSlide() {
-    indexSekarang++;
-    if (indexSekarang >= totalGambar) {
-        indexSekarang = 0; // Balik ke awal
-    }
-    updateSlider();
-}
+// function nextSlide() {
+//     indexSekarang++;
+//     if (indexSekarang >= totalGambar) {
+//         indexSekarang = 0; // Balik ke awal
+//     }
+//     updateSlider();
+// }
 
-function prevSlide() {
-    indexSekarang--;
-    if (indexSekarang < 0) {
-        indexSekarang = totalGambar - 1; // Lompat ke gambar terakhir
-    }
-    updateSlider();
-}
+// function prevSlide() {
+//     indexSekarang--;
+//     if (indexSekarang < 0) {
+//         indexSekarang = totalGambar - 1; // Lompat ke gambar terakhir
+//     }
+//     updateSlider();
+// }
+
+
---
+++ JS-Dasar/pert2/tugas2/index.js
@@ -0,0 +1,22 @@
+let slider = document.getElementsByClassName('slide')
+
+for (let i = 0 ; i < slider.length; i++){
+    console.log(slider[i]);
+    slider[i].style.display = 'none'
+    
+}
+
+// for (let j = 0; j < slider.index; j++){
+//    console.log(slider[j]);
+   
+    
+// }
+
+// fuindex  j++
+// ner() {
+//     slider[1].style.display = 'block'
+// }
+
+
+
+// gantibener()
