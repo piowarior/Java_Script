7b04fa08c9dfcc2beef4673263658b41116fb491
piowarior <muhamadrohisuliman@gmail.com>
2026-03-25 23:56:29 +0700

    update repo

--- nextjs/learnnext/app/Navbar.tsx
+++ nextjs/learnnext/app/Navbar.tsx
@@ -2,16 +2,17 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav>
-        <a href="/jajal"><img src="" alt="/" /> jajal</a>
-        <h2>MJD STORE</h2>
-        <div className="search"></div>
-        <ul>
-            <li><a className='bg-sky-600' href="/about">about</a></li>
+    <nav className='flex justify-center mt-5'>
+      <div className="flex w-[1500px] h-[50px] justify-around border-1 rounded-[5px] items-center ">
+        <a href="/jajal"> Logo</a>
+        <h2 className='-ml-50' > MJD STORE</h2>
+        <input type="search" placeholder='search' className='border w-[300px] pl-2 rounded-[5px]'/>
+        <ul className='flex gap-5 '>
+            <li><a href="/about">about</a></li>
             <li><a href="/services">services</a></li>
             <li><a href="/contact">contact</a></li>
-
         </ul>
+      </div>
     </nav>
   )
 }
